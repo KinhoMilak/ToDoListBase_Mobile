08b1c6026067d630296df7988a506f92dbf46757
Joao <kinho_milak@hotmail.com>
2024-04-08 20:51:26 -0300

    .

---
+++ app/src/main/java/br/edu/satc/todolistbase/CadLista.kt
@@ -0,0 +1,12 @@
+package br.edu.satc.todolistbase
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CadLista : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cad_lista)
+    }
+    //fun OnClick()
+}
